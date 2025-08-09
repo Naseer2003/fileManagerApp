1b9e962b6d2208005d8b7a1ef793ad2300c5589a
Naseer.K <naseerkovvuru2003@gmail.com>
2025-08-09 17:50:26 +0530

    almost done

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar: React.FC = () => {
     <div className="flex">
     <nav className="bg-[#2b2b2b] dark:bg-gray-900 text-white dark:text-gray-100 w-full flex items-center  px-6 py-2 shadow-md transition-colors duration-300">
       {/* Logo */}
-      <div className="flex flex-col leading-tight">
+      <div className="flex flex-col leading-tight cursor-pointer" onClick={() => navigate("/")}>
         <span className="font-bold text-lg tracking-wide">
           DEZIGN <span className="text-red-500">SHARK</span>
           <sup className="text-xs">®</sup>
